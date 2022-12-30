eab2df2838d0b63cc5875845c58231e582e0e8ae
Jarrod Bogard <jarrodbogard@gmail.com>
2022-12-29 20:33:19 -0600

    added firebase to project

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./store/reducers/rootReducer";
 import thunk from "redux-thunk";
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,6 @@
 export const createProject = (project) => {
   return (dispatch, getState) => {
-    // make asycn call to database
+    // make async call to database
     dispatch({
       type: "CREATE_PROJECT",
       payload: project,
